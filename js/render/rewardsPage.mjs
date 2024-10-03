1e167a23d4125e1208f895ca666fb905af6ecb8d
thislily <lilyclairewatson@gmail.com>
2024-10-03 18:33:33 +0200

    Update rewardsPage.mjs

--- js/render/rewardsPage.mjs
+++ js/render/rewardsPage.mjs
@@ -9,15 +9,34 @@ export function renderRewardsPage() {
 
     //create a youtube video window
     const video = document.createElement('iframe');
-    video.src = 'https://www.youtube.com/embed/tQQ4FWeaKWA?si=eKdpw_WWzyzKbTEx';
-    video.width = '800px';
-    video.height = '500px';
-    video.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+
+    if (userName === 'Oliver') {
+        video.src = 'https://www.youtube.com/embed/LDU_Txk06tM?si=NpDnQ4wnYNWipN3x';
+    } else if (userName === 'Casper') {
+        video.src = 'https://www.youtube.com/embed/X9XK9SVvp40?si=aXPP0TMZS-9FWF4d';
+    }
+    video.width = '900px';
+    video.height = '600px';
+    video.allow = 'autoplay;';
+    //video autoplays on load
+    video.autoplay = true;
+  
+
     video.allowFullscreen = true;
     video.style.border = 'none';
 
 
     videoContainer.appendChild(video);
+
+    //reset the chores in local storage for this user to all be competed:'false'
+
+    const localUsers = JSON.parse(localStorage.getItem('localUsers'));
+    const user = localUsers.find((user) => user.name === userName);
+    user.chores.forEach((chore) => {
+        chore.completed = false;
+    });
+    localStorage.setItem('localUsers', JSON.stringify(localUsers));
+    
 }
 
 
